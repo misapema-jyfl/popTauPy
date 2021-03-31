742a5c3ff9ae217544a4b72d347f785bf124273c
misapema-jyfl <misapema@jyu.fi>
2021-03-31 09:15:46 +0300

    Updated masterScript.py (opt_abc.py) * Set x and y axis lower limits to 0 when running opt_abc.py.

--- masterScript.py
+++ masterScript.py
@@ -88,6 +88,8 @@ def do_abc():
         ax.plot(T*1e3,y(T)*1e3,c="r", ls="--", label="Fitted")
         ax.set_xlabel("Time (ms)")
         ax.set_ylabel("Current (enA)")
+        ax.set_xlim(left=0)
+        ax.set_ylim(bottom=0)
         ax.legend()
         outName = "fig_fit_h{:.0e}_q{}".format(o["rk_time_step"], str(charge_state))     
         fig.tight_layout()    
