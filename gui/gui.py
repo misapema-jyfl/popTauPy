fbe5179290917838d307b3f1eb9ef9dad5a4886b
misapema-jyfl <misapema@jyu.fi>
2021-05-04 13:26:42 +0300

    Began work on GUI development.

---
+++ gui/gui.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 12:56:36 2021
+
+A Graphical User Interface for generating parameter .yaml files
+to run the Consecutive Transients Analyzer.
+
+@author: miha
+"""
+
+import tkinter as tk
+
